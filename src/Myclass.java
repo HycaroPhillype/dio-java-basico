537d6e8f26beaf79efdbd01d0023f9d0b8b641ff
Hycaro Phillype <phillypehycaro@gmail.com>
2025-03-12 09:13:21 -0300

    Anatomia das classes

---
+++ src/Myclass.java
@@ -0,0 +1,26 @@
+public class Myclass {
+    
+public static void main (String [] args) {
+    // String meunome = "hycaro";
+
+    // int anoFabricado = 2025;
+
+    // boolean simNao = true;
+    // anoFabricado = 2027;
+    
+    // int somar (int n1, int n2);
+
+    // String formatarCep(long cep);
+    String primeiroNome = "Hycaro";
+    String segundoNome = "Phillype";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto); // isso e para printar no terminal, 
+
+};
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return " Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+    
+}
---
+++ src/StudentBulletin.java
@@ -0,0 +1,11 @@
+public class StudentBulletin {
+    public static void main(String [] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+            System.out.println("Prova Minerva");
+        else
+            System.out.println("Aprovado");
+    }
+}
