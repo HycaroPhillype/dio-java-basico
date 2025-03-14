dc9b60c2c0e77f0b5530603d820d553cc4efe493
Hycaro Phillype <phillypehycaro@gmail.com>
2025-03-14 15:10:10 -0300

    Condionais compostas

---
+++ src/Atividades/CaixaEletronico.java
@@ -0,0 +1,39 @@
+package Atividades;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        double saldo = 2500.0;
+        
+        System.out.println("DIGITE SEU NOME POR FAVOR: ");
+        String nome = scanner.next();
+
+        System.out.println("Olá "
+            .concat(nome)
+            .concat(" Seu saldo atual é de: "
+            .concat(String.valueOf(saldo)))
+            .concat(" R$ reais, Quanto o senhor deseja sacar? "));
+
+        double valorSolicitado = scanner.nextDouble();
+
+        double saldoAtualizado = saldo - valorSolicitado;
+
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+            String mensagem = "Você sacou "
+                .concat(String.valueOf(valorSolicitado))
+                .concat(" R$ Reais do seu saldo. Saldo atualizdo: ")
+                .concat(String.valueOf(saldoAtualizado)
+                .concat(" R$ reais"));
+
+            System.out.println(mensagem);
+        }else
+            System.out.println("Desculpe, saldo insuficiente.");
+            
+    }
+    
+}
--- src/Atividades/MinhaClasse.java
+++ src/Atividades/MinhaClasse.java
@@ -1,3 +1,4 @@
+package Atividades;
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Olá, fui executado pelo terminal");
