25f2b30fae1d2db383658305ac35ff59e149debf
Hycaro Phillype <phillypehycaro@gmail.com>
2025-03-25 10:03:47 -0300

    Programação orientada a objetos

---
+++ src/Atividade2/SistemaCadastro.java
@@ -0,0 +1,15 @@
+package Atividade2;
+
+public class SistemaCadastro {
+    public static void main(String[] args) {
+        Pessoa marcos = new Pessoa("123", "MARCOS");
+
+        marcos.setEndereco("RUA DAS MARIAS");
+
+        System.out.println(marcos.getNome()
+        .concat(" - ")
+        .concat(marcos.getCpf()));
+
+    }
+
+}
---
+++ src/Atividade2/SistemaIbge.java
@@ -0,0 +1,17 @@
+package Atividade2;
+
+public class SistemaIbge {
+    public static void main(String[] args) {
+        for(EstadoBrasileiro e: EstadoBrasileiro.values()) {
+           System.out.println(e.getSigla().concat(" - ").concat(e.getNome())); 
+        }
+
+        EstadoBrasileiro eb = EstadoBrasileiro.MARANHAO;
+
+        System.out.println(eb.getNome());
+        System.out.println(eb.getSigla());
+        System.out.println(eb.getNomeMaisculo());
+        System.out.println(eb.getIbge());
+
+    }
+}
